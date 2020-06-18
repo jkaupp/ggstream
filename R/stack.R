d669f0bfceeb0e3fbb706d84a2f10a9511784619
Jake Kaupp <jake.kaupp@queensu.ca>
2020-06-18 12:22:48 -0400

    merged stack_densities and create_stacks into stack_streams

--- R/stack.R
+++ R/stack.R
@@ -1,6 +1,6 @@
-# @title stack_densities
-#
-# Takes density lines of equal x range and stack them on top of each other symmetrically aournd zero.
+# @title stack_streams
+
+# Takes a data frame of x and y values, creates streams and stack them on top of each other according to the particular method
 #
 # @param data a data frame
 # @param bw bandwidth of kernal density
@@ -12,7 +12,7 @@
 #
 #
 # @export
-stack_densities <- function(data, bw = bw, n_grid = n_grid, center_fun = center_fun, method = method){
+stack_streams <- function(data, bw = bw, n_grid = n_grid, center_fun = center_fun, method = method) {
 
   if (is.null(center_fun)) {
     center_fun <- function(x) {
@@ -21,92 +21,88 @@ stack_densities <- function(data, bw = bw, n_grid = n_grid, center_fun = center_
   }
 
   fun <- switch(method,
+                raw = make_connect_dots,
+                base = base,
                 density = make_smooth_density,
-                loess = make_smooth_loess,
-                raw = make_connect_dots)
+                themeRiver = themeRiver,
+                newWiggle = newWiggle,
+                minimizedWiggle = minimizedWiggle
+                )
 
   x_range <- range(data$x)
 
-  list <- lapply(split(data, data$group), fun, bw = bw, n_grid = n_grid, min_x = x_range[1], max_x = x_range[2])
+  if (method %in% c("raw", "density")) {
 
-  data <- do.call(rbind, list)
+    list <- lapply(split(data, data$group), fun, bw = bw, n_grid = n_grid, min_x = x_range[1], max_x = x_range[2])
 
-  data$group_tmp <- as.numeric(factor(data$group))
+    data <- do.call(rbind, list)
 
-  data <- data[order(data$x, data$group_tmp),]
+    data$group_tmp <- as.numeric(factor(data$group))
 
-  data_list <- lapply(split(data, data$x), calc_y_offsets)
+    data <- data[order(data$x, data$group_tmp),]
 
-  data <- do.call(rbind, data_list)
+    data_list <- lapply(split(data, data$x), calc_y_offsets)
 
-  data$ymin <- data$ymin + center_fun(data$x)
+    data <- do.call(rbind, data_list)
 
-  data$ymax <- data$ymax + center_fun(data$x)
+    data$ymin <- data$ymin + center_fun(data$x)
 
-  data <- lapply(split(data, data$group), extend_data)
+    data$ymax <- data$ymax + center_fun(data$x)
 
-  do.call(rbind, data)
+    data <- lapply(split(data, data$group), extend_data)
 
-}
-
-
-
-compute_stacks <- function(df, method = 'themeRiver') {
-
-  range_x <- range(df$x)
-
-  full_values <- data.frame(x = seq(range_x[1], range_x[2], 1/10^max(sapply(df$x, decimal_places))))
-
-  others <- df[!names(df) %in% c("x", "y")]
-
-  sub_df <- df[names(df) %in% c("x", "y", "group")]
-
-  list <- split(sub_df, df$group)
-
-  list <- lapply(list, function(x) merge(full_values, x, by = "x", all.x = TRUE))
-
-  list <- lapply(list,  replace_values)
-
-  list <- lapply(list, function(x) setNames(x, c("x", unique(as.character(x$group)), "group")))
-
-  list <- lapply(list, function(x) x[names(x) != "group"])
-
-  values <- as.matrix(Reduce(function(x, y) merge(x, y, by = "x"), list))
-
-  col_names <- colnames(values)
-
-  xval <- values[, 1]
-
-  values <- as.matrix(values[, -1])
-
-  colnames(values) <- col_names[-1]
-
-  dims <- dim(values)
-
-  if (is.null(dims[1])) {
-
-    timePoints <- length(values)
-    nStreams <- 1
-    values <- as.matrix(values)
+    out <- do.call(rbind, data)
 
   } else {
 
-    timePoints <- dims[1]
-    nStreams <- dim(values)[2]
+    full_values <- data.frame(x = seq(x_range[1], x_range[2], 1/10^max(sapply(data$x, decimal_places))))
+
+    others <- data[!names(data) %in% c("x", "y")]
+
+    sub_df <- data[names(data) %in% c("x", "y", "group")]
+
+    list <- split(sub_df, data$group)
+
+    list <- lapply(list, function(x) merge(full_values, x, by = "x", all.x = TRUE))
+
+    list <- lapply(list,  replace_values)
+
+    list <- lapply(list, function(x) setNames(x, c("x", unique(as.character(x$group)), "group")))
+
+    list <- lapply(list, function(x) x[names(x) != "group"])
+
+    values <- as.matrix(Reduce(function(x, y) merge(x, y, by = "x"), list))
+
+    col_names <- colnames(values)
+
+    xval <- values[, 1]
+
+    values <- as.matrix(values[, -1])
+
+    colnames(values) <- col_names[-1]
+
+    dims <- dim(values)
+
+    if (is.null(dims[1])) {
+
+      timePoints <- length(values)
+      nStreams <- 1
+      values <- as.matrix(values)
+
+    } else {
+
+      timePoints <- dims[1]
+      nStreams <- dim(values)[2]
+
+    }
+
+    out <- do.call(fun, list(values = values, timePoints = timePoints, nStreams = nStreams))
+
+    out <- merge(out, unique(others))
+
+    out$x <- rep(c(xval, rev(xval)), length(unique(out$group)))
 
   }
 
-  out <- switch(method,
-                base = base(values, timePoints, nStreams),
-                themeRiver = themeRiver(values, timePoints, nStreams),
-                newWiggle = newWiggle(values, timePoints, nStreams),
-                minimizedWiggle = minimizedWiggle(values, timePoints, nStreams))
-
-  out <- merge(out, unique(others))
-
-  out$x <- rep(c(xval, rev(xval)), length(unique(out$group)))
-
-  out
-
-
+  return(out)
 }
